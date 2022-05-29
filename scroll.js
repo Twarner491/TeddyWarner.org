11dfecaa2443e380df05d6eb474f1885fec08483
Teddy Warner <Twarner491@gmail.com>
2022-05-29 13:54:05 -0400

    scroll

---
+++ scroll.js
@@ -0,0 +1,8 @@
+//import LocomotiveScroll from 'locomotive-scroll';
+
+//const scroll = new LocomotiveScroll({
+//    el: document.querySelector('[data-scroll-container]'),
+//    smooth: true
+//});
+
+console.log('test')
